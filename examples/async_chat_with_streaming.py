b543e388cdf8adc8e360ad5a5103e68ae9937da4
Theophile Gervet <theophile.gervet@gmail.com>
2023-12-08 11:34:15 +0100

    Split examples

---
+++ examples/async_chat_with_streaming.py
@@ -0,0 +1,22 @@
+import asyncio
+import os
+
+from mistral_client.async_client import MistralAsyncClient
+from mistral_client.models.chat_completion import ChatMessage
+
+
+async def main():
+    api_key = os.environ["MISTRAL_API_KEY"]
+    model = "mistral-tiny"
+
+    client = MistralAsyncClient(api_key=api_key)
+
+    async for chunk in client.chat_stream(
+        model=model,
+        messages=[ChatMessage(role="user", content="What is the best French cheese?")],
+    ):
+        print(chunk)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
