b13f1a322a3887e2d09b33a05829ac2466cfb74f
gaspardBT <gaspard@mistral.ai>
2024-08-16 18:55:17 +0200

    fix example

--- examples/async_jobs.py
+++ examples/async_jobs.py
@@ -10,9 +10,7 @@ from mistralai.models import File, TrainingParametersIn
 async def main():
     api_key = os.environ["MISTRAL_API_KEY"]
 
-    client = Mistral(
-        api_key="gpN4hC0YOSdZoBbzRcWNyALyMnNOT9jj", server_url="http://0.0.0.0:8882/"
-    )
+    client = Mistral(api_key=api_key)
 
     # Create new files
     with open("examples/file.jsonl", "rb") as f:
