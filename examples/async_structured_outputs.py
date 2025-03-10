e03d38bfa4e2146f2bb6b84f2d40c34ae4db8a42
Alexandre Menasria <alexandre.menasria@mistral.ai>
2025-03-10 10:24:01 +0100

    [CI] Fix the CI for Python 3.8

--- examples/async_structured_outputs.py
+++ examples/async_structured_outputs.py
@@ -5,9 +5,10 @@ import os
 from pydantic import BaseModel
 
 from mistralai import Mistral
+from typing import List
+
 
 async def main():
-
     api_key = os.environ["MISTRAL_API_KEY"]
     client = Mistral(api_key=api_key)
 
@@ -16,18 +17,22 @@ async def main():
         output: str
 
     class MathDemonstration(BaseModel):
-        steps: list[Explanation]
+        steps: List[Explanation]
         final_answer: str
 
     chat_response = await client.chat.parse_async(
         model="mistral-large-2411",
         messages=[
-            {"role": "system", "content": "You are a helpful math tutor. You will be provided with a math problem, and your goal will be to output a step by step solution, along with a final answer. For each step, just provide the output as an equation use the explanation field to detail the reasoning."},
+            {
+                "role": "system",
+                "content": "You are a helpful math tutor. You will be provided with a math problem, and your goal will be to output a step by step solution, along with a final answer. For each step, just provide the output as an equation use the explanation field to detail the reasoning.",
+            },
             {"role": "user", "content": "How can I solve 8x + 7 = -23"},
         ],
-        response_format = MathDemonstration
+        response_format=MathDemonstration,
     )
     print(chat_response.choices[0].message.parsed)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
