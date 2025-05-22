18d0ba0c82245fefb4b1d0a57ad8ae60aa7f3a94
Jean-Malo Delignon <56539593+jean-malo@users.noreply.github.com>
2025-05-22 16:57:08 +0200

    Revert "chore: 🐝 Update SDK - Generate MISTRALAI MISTRALAI-SDK 1.7.1 (#220)" (#222)
    This reverts commit be02ba7ffa4ce2e5d290424684eba8ce08f40c80.

--- src/mistralai/_version.py
+++ src/mistralai/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "mistralai"
-__version__: str = "1.7.1"
-__openapi_doc_version__: str = "1.0.0"
+__version__: str = "1.7.0"
+__openapi_doc_version__: str = "0.0.2"
 __gen_version__: str = "2.548.6"
-__user_agent__: str = "speakeasy-sdk/python 1.7.1 2.548.6 1.0.0 mistralai"
+__user_agent__: str = "speakeasy-sdk/python 1.7.0 2.548.6 0.0.2 mistralai"
 
 try:
     if __package__ is not None:
