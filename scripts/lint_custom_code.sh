69374e89bdc7696968e9bccf7209486a1c4945d1
Alexandre Menasria <47357713+amenasria@users.noreply.github.com>
2025-05-26 16:47:13 +0200

    Lint with `ruff`, `pyright` and `mypy` the `examples` and `extra` folders (#226)

---
+++ scripts/lint_custom_code.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+ERRORS=0
+
+echo "Running mypy..."
+# TODO: Uncomment once the examples are fixed
+# poetry run mypy examples/ || ERRORS=1
+poetry run mypy src/mistralai/extra/ || ERRORS=1
+poetry run mypy src/mistralai/_hooks/ \
+--exclude __init__.py --exclude sdkhooks.py --exclude types.py || ERRORS=1
+
+echo "Running pyright..."
+# TODO: Uncomment once the examples are fixed
+# poetry run pyright examples/ || ERRORS=1
+poetry run pyright src/mistralai/extra/ || ERRORS=1
+poetry run pyright src/mistralai/_hooks/ || ERRORS=1
+
+echo "Running ruff..."
+poetry run ruff check examples/ || ERRORS=1
+poetry run ruff check src/mistralai/extra/ || ERRORS=1
+poetry run ruff check src/mistralai/_hooks/ \
+--exclude __init__.py --exclude sdkhooks.py --exclude types.py || ERRORS=1
+
+if [ "$ERRORS" -ne 0 ]; then
+echo "❌ One or more linters failed"
+exit 1
+else
+echo "✅ All linters passed"
+fi
