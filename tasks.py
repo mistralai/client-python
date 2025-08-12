864ceca714f666913f4dec6b4473d672993739e5
Alexandre Menasria <47357713+amenasria@users.noreply.github.com>
2025-08-12 15:48:39 +0200

    Speakeasy update workflow (#254)
    * Add the update_speakeasy.py script
    
    * Fix linter errors
    
    * Make it an invoke task
    
    * Rename scripts/update_speakeasy.py -> utils/speakeasy.py
    
    * Add GHA Workflow
    
    * Add missing permissions
    
    * Revert python >= 3.10

---
+++ tasks.py
@@ -0,0 +1,48 @@
+import re
+from invoke.context import Context
+from invoke.tasks import task
+from utils.speakeasy import (
+    pin_speakeasy_version,
+    OpenAPISpecsPinned,
+    SpeakeasyTargets,
+    WORKFLOW_PATH,
+    WORKFLOW_LOCK_PATH,
+)
+
+
+@task(iterable=["targets"])
+def update_speakeasy(
+    ctx: Context,
+    version: str,
+    targets: list[SpeakeasyTargets] = [SpeakeasyTargets.ALL],
+    workflow_path: str = WORKFLOW_PATH,
+    workflow_lock_path: str = WORKFLOW_LOCK_PATH,
+    verbose: bool = False,
+):
+    if not re.match(r'^\d+\.\d+\.\d+$', version):
+        raise ValueError(f"Invalid version format: {version}. Expected format: X.Y.Z (e.g., 1.2.3)")
+    """
+    Update the speakeasy version and pin the openapi specs to the current revision.
+
+    Usage:
+        inv update-speakeasy --version "1.580.2" --targets "all"
+        inv update-speakeasy --version "1.580.2" --targets "mistralai-azure-sdk" --targets "mistralai-gcp-sdk" --verbose
+        inv update-speakeasy --version "1.580.2" --targets "mistralai-sdk" --workflow-path ".speakeasy/workflow.yaml" --workflow-lock-path ".speakeasy/workflow.lock.yaml"
+        inv update-speakeasy --version "1.580.2" --targets "mistralai-sdk" --workflow-path ".speakeasy/workflow.yaml" --workflow-lock-path ".speakeasy/workflow.lock.yaml" --verbose
+    """
+    for target in targets:
+        try:
+            SpeakeasyTargets(target)
+        except ValueError:
+            raise ValueError(
+                f"Invalid target: {target}. Your targets must be one of {SpeakeasyTargets.list()}"
+            )
+    cmd = (
+        "speakeasy run"
+        + " --skip-versioning"
+        + "".join(f" -t {target}" for target in targets)
+        + (" --verbose" if verbose else "")
+    )
+    pin_speakeasy_version(workflow_path=workflow_path, version=version)
+    with OpenAPISpecsPinned(workflow_path, workflow_lock_path):
+        ctx.run(cmd)
---
+++ utils/speakeasy.py
@@ -0,0 +1,91 @@
+"""
+This script:
+- pins the OpenAPI specs,
+- runs speakeasy to update the SDKs' files,
+- and then unpins the OpenAPI specs.
+
+It is advised to often run this script to avoid getting unrelated changes (due to updates) when modifying the OpenAPI specs.
+"""
+
+import yaml
+from io import TextIOWrapper
+import copy
+import subprocess
+from enum import Enum
+
+WORKFLOW_PATH = ".speakeasy/workflow.yaml"
+WORKFLOW_LOCK_PATH = ".speakeasy/workflow.lock"
+
+
+def set_location_rev(yaml_content: dict, source_name: str, new_rev: str) -> None:
+    registry = yaml_content["sources"][source_name]["inputs"][0]["location"].split(":")[0]
+    yaml_content["sources"][source_name]["inputs"][0]["location"] = f"{registry}:{new_rev}"
+
+
+def write_yaml(yaml_content: dict, file: TextIOWrapper) -> None:
+    return yaml.dump(
+        yaml_content, file, default_flow_style=False, sort_keys=False, indent=4
+    )
+
+def pin_speakeasy_version(workflow_path: str, version: str):
+    with open(workflow_path, "r") as file:
+        workflow_yaml = yaml.safe_load(file)
+    workflow_yaml["speakeasyVersion"] = version
+    with open(workflow_path, "w") as file:
+        write_yaml(workflow_yaml, file)
+
+class OpenAPISpecsPinned:
+    def __init__(self, workflow_path: str, workflow_lock_path: str):
+        self.workflow_path = workflow_path
+        self.workflow_lock_path = workflow_lock_path
+        with open(workflow_path, "r") as file:
+            self.workflow_yaml = yaml.safe_load(file)
+
+    def __enter__(self):
+        print("OpenAPI specs pinned to current revision")
+        self.pin_to_current_rev()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.unpin()
+        print("OpenAPI specs unpinned")
+
+    def pin_to_current_rev(self):
+        yaml_copy = copy.deepcopy(self.workflow_yaml)
+        # Getting the current revisions of the OpenAPI specs
+        with open(self.workflow_lock_path, "r") as lock_file:
+            yaml_lock = yaml.safe_load(lock_file)
+            rev_azure = yaml_lock["sources"]["mistral-azure-source"]["sourceRevisionDigest"]
+            rev_google_cloud = yaml_lock["sources"]["mistral-google-cloud-source"]["sourceRevisionDigest"]
+            rev_mistralai = yaml_lock["sources"]["mistral-openapi"]["sourceRevisionDigest"]
+
+        # Pinning the OpenAPI specs to the current revisions
+        with open(self.workflow_path, "w") as file:
+            set_location_rev(yaml_copy, "mistral-azure-source", rev_azure)
+            set_location_rev(yaml_copy, "mistral-google-cloud-source", rev_google_cloud)
+            set_location_rev(yaml_copy, "mistral-openapi", rev_mistralai)
+            write_yaml(yaml_content=yaml_copy, file=file)
+
+
+    def unpin(self):
+        with open(self.workflow_path, "w") as file:
+            write_yaml(yaml_content=self.workflow_yaml, file=file)
+
+class SpeakeasyTargets(str, Enum):
+    """
+    The list of targets defined in the .speakeasy/workflow.yaml[.targets] section.
+    This can also be listed running `speakeasy list targets` in the root of the project.
+    """
+    ALL = "all"
+    MISTRALAI_SDK = "mistralai-sdk"
+    MISTRALAI_AZURE_SDK = "mistralai-azure-sdk"
+    MISTRALAI_GCP_SDK = "mistralai-gcp-sdk"
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
+
+
+if __name__ == "__main__":
+    pin_speakeasy_version(workflow_path=WORKFLOW_PATH, version="1.580.2")
+    with OpenAPISpecsPinned(WORKFLOW_PATH, WORKFLOW_LOCK_PATH):
+        subprocess.run(["speakeasy", "run", "-t", "mistralai-sdk", "--skip-versioning", "--verbose"])
