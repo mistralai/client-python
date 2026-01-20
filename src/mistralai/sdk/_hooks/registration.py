ed7f7bca832a5dd8cc452380f366cc3848e0b3ae
Vishal Gowda <cartmanboy1991@gmail.com>
2026-01-20 12:26:24 +0000

    fix: restore custom hooks for namespace package structure
    Migrate custom hooks (custom_user_agent, deprecation_warning, tracing)
    to new sdk/_hooks location. Update import path in tracing.py to use
    absolute import for mistralai.extra.observability.otel.

--- src/mistralai/sdk/_hooks/registration.py
+++ src/mistralai/sdk/_hooks/registration.py
@@ -1,5 +1,7 @@
+from .custom_user_agent import CustomUserAgentHook
+from .deprecation_warning import DeprecationWarningHook
+from .tracing import TracingHook
 from .types import Hooks
-
 
 # This file is only ever generated once on the first generation and then is free to be modified.
 # Any hooks you wish to add should be registered in the init_hooks function. Feel free to define them
@@ -10,4 +12,11 @@ def init_hooks(hooks: Hooks):
     # pylint: disable=unused-argument
     """Add hooks by calling hooks.register{sdk_init/before_request/after_success/after_error}Hook
     with an instance of a hook that implements that specific Hook interface
-    Hooks are registered per SDK instance, and are valid for the lifetime of the SDK instance"""
+    Hooks are registered per SDK instance, and are valid for the lifetime of the SDK instance
+    """
+    tracing_hook = TracingHook()
+    hooks.register_before_request_hook(CustomUserAgentHook())
+    hooks.register_after_success_hook(DeprecationWarningHook())
+    hooks.register_after_success_hook(tracing_hook)
+    hooks.register_before_request_hook(tracing_hook)
+    hooks.register_after_error_hook(tracing_hook)
