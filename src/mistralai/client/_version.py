64c23a184452a2453a7aff808d7901d0e1718483
Nicolas Faurie <nicolas.faurie@mistral.ai>
2026-04-14 14:14:36 +0200

    Fix generation conflict (#486)
    * Fix generation conflict
    
    * Fix

--- src/mistralai/client/_version.py
+++ src/mistralai/client/_version.py
@@ -4,10 +4,10 @@
 import importlib.metadata
 
 __title__: str = "mistralai"
-__version__: str = "2.3.6"
+__version__: str = "2.3.8"
 __openapi_doc_version__: str = "1.0.0"
 __gen_version__: str = "2.879.6"
-__user_agent__: str = "speakeasy-sdk/python 2.3.6 2.879.6 1.0.0 mistralai"
+__user_agent__: str = "speakeasy-sdk/python 2.3.8 2.879.6 1.0.0 mistralai"
 
 try:
     if __package__ is not None:
