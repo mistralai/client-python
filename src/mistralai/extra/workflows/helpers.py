c177adac137159104539bd7bec767322b5db16af
Nicolas Faurie <nicolas.faurie@mistral.ai>
2026-05-21 16:50:23 +0200

    Fix worklfow helper

--- src/mistralai/extra/workflows/helpers.py
+++ src/mistralai/extra/workflows/helpers.py
@@ -70,7 +70,7 @@ async def get_scheduler_namespace(
             ),
         ),
         request=req,
-        error_status_codes=["4XX", "5XX"],
+        is_error_status_code=lambda c: utils.match_status_codes(["4XX", "5XX"], c),
         retry_config=retry_config,
     )
 
