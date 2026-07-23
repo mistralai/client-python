3c5084828e398a7002305abb3230c34caa3a0973
Tanmay Kale <tanmay.kale@macbook-pro-tanmaykale.cheetah-koi.ts.net>
2026-07-23 17:24:46 +0200

    fix: lower default streaming read timeout to 20s [AIR-1092]

--- src/mistralai/client/fim.py
+++ src/mistralai/client/fim.py
@@ -333,7 +333,7 @@ class Fim(BaseSDK):
             timeout_ms = self.sdk_configuration.timeout_ms
 
         if timeout_ms is None:
-            timeout_ms = 300000
+            timeout_ms = 20000
 
         if server_url is not None:
             base_url = server_url
@@ -475,7 +475,7 @@ class Fim(BaseSDK):
             timeout_ms = self.sdk_configuration.timeout_ms
 
         if timeout_ms is None:
-            timeout_ms = 300000
+            timeout_ms = 20000
 
         if server_url is not None:
             base_url = server_url
